55956219c1c29211d16770919567a8187aff6b0d
chanakyha <vetrichanakyha2003@gmail.com>
2023-09-20 10:38:02 +0530

    Contact and Courses Offered Added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,66 +8,66 @@ import Icon from "../../assets/icon.png";
 // import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import Link from "next/link";
 import {
-    FacebookIcon,
-    InstagramIcon,
-    LinkedinIcon,
-    TwitterIcon,
-    YoutubeIcon,
+  FacebookIcon,
+  InstagramIcon,
+  LinkedinIcon,
+  TwitterIcon,
+  YoutubeIcon,
 } from "lucide-react";
 
 const Footer = () => {
-    return (
-        <footer id="contact" className="bg-[#f6f7fc]">
-            <div className="mx-auto max-w-[1440px] px-[12px] pt-[72px] md:px-[20px] lg:px-[5vw]">
-                <div className="flex flex-col items-stretch justify-start">
-                    <div className="w-full border-b p-2 md:p-4">
-                        <div className="flex flex-col items-center justify-between md:items-start lg:w-full lg:flex-row">
-                            <div className="flex flex-col md:flex-row items-center lg:w-full mb-[40px]">
-                                <Link
-                                    href="/"
-                                    className="flex flex-col items-center"
-                                >
-                                    <Image
-                                        src={"/assets/srmlogo.webp"}
-                                        alt="SRMIST Logo"
-                                        width={1000}
-                                        height={1000}
-                                        className=" h-auto w-56 md:ml-[12px] lg:mb-0"
-                                    />
-                                    <h1 className=" text-2xl tracking-widest font-bold lg:mb-0">
-                                        ICACST &apos;23
-                                    </h1>
-                                </Link>
-                                <div className="text-center lg:text-left lg:w-96">
-                                    <p className="font-semibold">
-                                        SRM Institute of Science and Technology
-                                        <br></br>
-                                        Ramapuram Campus
-                                    </p>
-                                    <p className="text-sm mt-2">
-                                        Bharathi Salai, Ramapuram, Chennai – 600
-                                        089.
-                                    </p>
-                                    <div className="flex flex-col mt-2">
-                                        <Link
-                                            className="hover:underline"
-                                            href={
-                                                "mailto:helpdesk@srmrmp.edu.in"
-                                            }
-                                        >
-                                            helpdesk@srmrmp.edu.in
-                                        </Link>
-                                        <Link
-                                            className="hover:underline"
-                                            href={"tel:1800 102 1525"}
-                                        >
-                                            1800 102 1525
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="flex w-full flex-col items-center justify-between md:flex-row lg:justify-end">
-                                {/* <div className="mb-4 flex flex-col items-stretch justify-start text-center">
+  return (
+    <footer id="contact" className="bg-[#f6f7fc]">
+      <div className="mx-auto max-w-[1440px] px-[12px] pt-[72px] md:px-[20px] lg:px-[5vw]">
+        <div className="flex flex-col items-stretch justify-start">
+          <div className="w-full border-b p-2 md:p-4">
+            <div className="flex flex-col items-center justify-between md:items-start lg:w-full lg:flex-row">
+              <div className="flex flex-col md:flex-row items-center lg:w-full mb-[40px]">
+                <Link href="/" className="flex flex-col items-center">
+                  <Image
+                    src={"/assets/srmlogo.webp"}
+                    alt="SRMIST Logo"
+                    width={1000}
+                    height={1000}
+                    className=" h-auto w-56 md:ml-[12px] lg:mb-0"
+                  />
+                  <h1 className=" text-2xl tracking-widest font-bold lg:mb-0">
+                    ICACST &apos;23
+                  </h1>
+                </Link>
+                <div className="text-center lg:text-left lg:w-96">
+                  <p className="font-semibold">
+                    SRM Institute of Science and Technology
+                    <br></br>
+                    Ramapuram Campus
+                  </p>
+                  <p className="text-sm mt-2">
+                    Bharathi Salai, Ramapuram, Chennai – 600 089.
+                  </p>
+                  <div className="flex flex-col mt-2">
+                    <p className="text-sm font-semibold">For Queries:</p>
+
+                    <p>Dr.M.Ayyadurai /Asst. Prof/CSE</p>
+                    <Link
+                      className="hover:underline"
+                      href={"mailto:rmpicacst@srmist.edu.in"}
+                    >
+                      rmpicacst@srmist.edu.in
+                    </Link>
+                    <p className="flex justify-center lg:justify-start items-center gap-2">
+                      Mobile no:
+                      <Link
+                        className="hover:underline"
+                        href={"tel:+917708911254"}
+                      >
+                        +91 77089 11254
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-full flex-col items-center justify-between md:flex-row lg:justify-end">
+                {/* <div className="mb-4 flex flex-col items-stretch justify-start text-center">
                                     <a className="p-3 text-[14px] md:px-3 md:py-4 lg:mx-3 lg:py-2 ">
                                         SRM Institute of Science and Technology
                                         Ramapuram Campus
@@ -82,91 +82,91 @@ const Footer = () => {
                                         FAQs
                                     </a>
                                 </div> */}
-                                <div className="mb-4 flex items-center justify-start lg:ml-[64px]">
-                                    <Link
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        href="https://www.facebook.com/srmist.rmp/"
-                                        className="mx-[2px] p-3 transition-all duration-300 hover:text-cyan-400"
-                                    >
-                                        <FacebookIcon strokeWidth={1.5} />
-                                    </Link>
-                                    <Link
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        href="https://www.linkedin.com/company/35950519/"
-                                        className="mx-[2px] p-3 transition-all duration-300 hover:text-cyan-400"
-                                    >
-                                        <LinkedinIcon strokeWidth={1.5} />
-                                    </Link>
-                                    <Link
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        href="https://twitter.com/srmistramapuram"
-                                        className="mx-[2px] p-3 transition-all duration-300 hover:text-cyan-400"
-                                    >
-                                        <TwitterIcon strokeWidth={1.5} />
-                                    </Link>
-                                    <Link
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        href="https://www.instagram.com/srmist.rmp/"
-                                        className="mx-[2px] p-3 transition-all duration-300 hover:text-cyan-400"
-                                    >
-                                        <InstagramIcon strokeWidth={1.5} />
-                                    </Link>
-                                    <Link
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        href="https://www.youtube.com/channel/UCPiZwXRaA2NMNQ0xdDmzpmA"
-                                        className="mx-[2px] p-3 transition-all duration-300 hover:text-cyan-400"
-                                    >
-                                        <YoutubeIcon strokeWidth={1.5} />
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-full p-2 md:p-4">
-                        <div className="flex flex-col items-center justify-start md:flex-row md:justify-between">
-                            <p className="text-[12px] text-[#777a85]">
-                                Copyright &copy; {new Date().getFullYear()} SRM
-                                Institute of Science and technology, Ramapuram
-                            </p>
-                            <div className="mb-4 flex flex-col items-stretch justify-center text-center md:flex-row">
-                                <Link
-                                    target="_blank"
-                                    href="https://srmrmp.edu.in/terms-conditions/"
-                                    className="py-2 text-[14px] text-[#777a85] md:mr-[24px]"
-                                >
-                                    Terms and Conditions
-                                </Link>
-                                <Link
-                                    target="_blank"
-                                    href="https://srmrmp.edu.in/privacy-policy-2-2/"
-                                    className="py-2 text-[14px] text-[#777a85] md:mr-[24px]"
-                                >
-                                    Privacy Policy
-                                </Link>
-                            </div>
-                        </div>
-                        <div className="w-full text-center">
-                            <span className="text-xs text-slate-500">
-                                Designed and Developed by{" "}
-                                <Link
-                                    target="_blank"
-                                    className="hover:underline hover:text-blue-500"
-                                    href={"https://linktr.ee/tech_takaliss"}
-                                >
-                                    Students of SRMRMP
-                                </Link>
-                            </span>
-                        </div>
-                    </div>
+                <div className="mb-4 flex items-center justify-start lg:ml-[64px]">
+                  <Link
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.facebook.com/srmist.rmp/"
+                    className="mx-[2px] p-3 transition-all duration-300 hover:text-cyan-400"
+                  >
+                    <FacebookIcon strokeWidth={1.5} />
+                  </Link>
+                  <Link
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.linkedin.com/company/35950519/"
+                    className="mx-[2px] p-3 transition-all duration-300 hover:text-cyan-400"
+                  >
+                    <LinkedinIcon strokeWidth={1.5} />
+                  </Link>
+                  <Link
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://twitter.com/srmistramapuram"
+                    className="mx-[2px] p-3 transition-all duration-300 hover:text-cyan-400"
+                  >
+                    <TwitterIcon strokeWidth={1.5} />
+                  </Link>
+                  <Link
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.instagram.com/srmist.rmp/"
+                    className="mx-[2px] p-3 transition-all duration-300 hover:text-cyan-400"
+                  >
+                    <InstagramIcon strokeWidth={1.5} />
+                  </Link>
+                  <Link
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.youtube.com/channel/UCPiZwXRaA2NMNQ0xdDmzpmA"
+                    className="mx-[2px] p-3 transition-all duration-300 hover:text-cyan-400"
+                  >
+                    <YoutubeIcon strokeWidth={1.5} />
+                  </Link>
                 </div>
+              </div>
             </div>
-        </footer>
-    );
+          </div>
+          <div className="w-full p-2 md:p-4">
+            <div className="flex flex-col items-center justify-start md:flex-row md:justify-between">
+              <p className="text-[12px] text-[#777a85]">
+                Copyright &copy; {new Date().getFullYear()} SRM Institute of
+                Science and technology, Ramapuram
+              </p>
+              <div className="mb-4 flex flex-col items-stretch justify-center text-center md:flex-row">
+                <Link
+                  target="_blank"
+                  href="https://srmrmp.edu.in/terms-conditions/"
+                  className="py-2 text-[14px] text-[#777a85] md:mr-[24px]"
+                >
+                  Terms and Conditions
+                </Link>
+                <Link
+                  target="_blank"
+                  href="https://srmrmp.edu.in/privacy-policy-2-2/"
+                  className="py-2 text-[14px] text-[#777a85] md:mr-[24px]"
+                >
+                  Privacy Policy
+                </Link>
+              </div>
+            </div>
+            <div className="w-full text-center">
+              <span className="text-xs text-slate-500">
+                Designed and Developed by{" "}
+                <Link
+                  target="_blank"
+                  className="hover:underline hover:text-blue-500"
+                  href={"https://linktr.ee/tech_takaliss"}
+                >
+                  Students of SRMRMP
+                </Link>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/landing/About.tsx
+++ components/landing/About.tsx
@@ -3,104 +3,115 @@ import { Button } from "../ui/button";
 import Link from "next/link";
 
 function About() {
-    return (
-        <section className="p-4 md:px-16 lg:p-0 lg:max-w-6xl lg:mx-auto">
-            <div className="flex flex-col lg:items-center lg:w-full gap-4 my-10 lg:my-20">
-                <h1 className="text-lg md:text-xl lg:text-2xl border-b-2 pb-1 lg:pb-2 border-gray-800 w-fit font-bold tracking-wider">
-                    Scope of ICACST
-                </h1>
-                <p className="text-sm md:text-base text-slate-500 font-medium text-justify">
-                    The International Conference on Advances in Computer Science
-                    and Technologies (ICACST) is a premier forum for the
-                    presentation of new advances and research results in the
-                    field of Computer Science and Technologies. The conference
-                    will bring together leading academic scientists, researchers
-                    and scholars in the domain of interest from around the
-                    world.
-                </p>
-            </div>
-            <div className="flex flex-col lg:items-center lg:w-full gap-4 my-10 lg:my-20">
-                <h1 className="text-lg md:text-xl lg:text-2xl border-b-2 pb-1 lg:pb-2 border-gray-800 w-fit font-bold tracking-wider">
-                    About SRMIST
-                </h1>
-                <p className="text-sm md:text-base text-slate-500 font-medium text-justify">
-                    SRM Institute of Science and Technology (formerly known as
-                    SRM University) is one of the top ranking universities in
-                    India with over 20,000 students and 1,500 faculty, offering
-                    a wide range of undergraduate, postgraduate and doctoral
-                    programs in Engineering, Management, Medicine and Health
-                    sciences, Science and Humanities.
-                </p>
-                <Link
-                    className="self-end"
-                    href="https://srmrmp.edu.in/"
-                    target={"_blank"}
-                >
-                    <Button variant={"outline"}>More About SRM</Button>
-                </Link>
-            </div>
-            <div className="flex flex-col lg:items-center lg:w-full gap-4 my-10 lg:my-20">
-                <h1 className="text-lg md:text-xl lg:text-2xl border-b-2 pb-1 lg:pb-2 border-gray-800 w-fit font-bold tracking-wider">
-                    About CSE
-                </h1>
-                <p className="text-sm md:text-base text-slate-500 font-medium text-justify">
-                    The department of Computer Science and Engineering (CSE) at
-                    SRM Institute of Science and Technology was established in
-                    the year 2004 with the aim of imparting quality education to
-                    students and bring out the best in them. The key goal of the
-                    department is to provide best IT infrastructure, world class
-                    learning and research environment, adopt industry practices
-                    through industry collaborations and inculcate moral and
-                    ethical values. With students hailing from all States and
-                    union territories of India, the department was established
-                    to meet the demand for well-qualified computer
-                    professionals. The promptness of the students to learn makes
-                    it easier for the industry-trained experienced faculty to
-                    produce top-notch engineers who are being recruited by
-                    reputed companies all over the world. The department is
-                    renowned for imparting state of the art undergraduate
-                    education and preparing its students for real world
-                    challenges. The department is accredited by NAAC as Grade
-                    A++ and placed by MHRD in category A. Our alumni have done
-                    extremely well which include managing top companies,
-                    designing revolutionary products, and contributing to
-                    fundamental research.
-                </p>
-                <h1 className="font-bold tracking-wider self-start text-lg md:text-xl lg:text-2xl mt-10">
-                    Vision
-                </h1>
-                <p className="text-sm md:text-base text-slate-500 font-medium text-justify">
-                    To become a world class department in imparting high quality
-                    knowledge and in providing students a unique learning and
-                    research experience in Computer Science and Engineering.
-                </p>
-                <h1 className="text-lg md:text-xl lg:text-2xl mt-10 self-start font-bold tracking-wider">
-                    Mission
-                </h1>
-                <ul className="list-disc self-start">
-                    <li className="text-sm md:text-base text-slate-500 font-medium text-justify">
-                        To impart knowledge in cutting edge technologies on part
-                        with industrial standards.
-                    </li>
-                    <li className="text-sm md:text-base text-slate-500 font-medium text-justify">
-                        To collaborate with renowned academic institutions in
-                        research and development.
-                    </li>
-                    <li className="text-sm md:text-base text-slate-500 font-medium text-justify">
-                        To instill societal and ethical responsibilities in all
-                        professional activities.
-                    </li>
-                </ul>
-                <Link
-                    className="self-end"
-                    href="https://srmrmp.edu.in/academics/department-of-computer-science-and-engineering/"
-                    target={"_blank"}
-                >
-                    <Button variant={"outline"}>More About CSE</Button>
-                </Link>
-            </div>
-        </section>
-    );
+  return (
+    <section className="p-4 md:px-16 lg:p-0 lg:max-w-6xl lg:mx-auto">
+      <div className="flex flex-col lg:items-center lg:w-full gap-4 my-10 lg:my-20">
+        <h1 className="text-lg md:text-xl lg:text-2xl border-b-2 pb-1 lg:pb-2 border-gray-800 w-fit font-bold tracking-wider">
+          Scope of ICACST
+        </h1>
+        <p className="text-sm md:text-base text-slate-500 font-medium text-justify">
+          The International Conference on Advances in Computer Science and
+          Technologies (ICACST) is a premier forum for the presentation of new
+          advances and research results in the field of Computer Science and
+          Technologies. The conference will bring together leading academic
+          scientists, researchers and scholars in the domain of interest from
+          around the world.
+        </p>
+      </div>
+      <div className="flex flex-col lg:items-center lg:w-full gap-4 my-10 lg:my-20">
+        <h1 className="text-lg md:text-xl lg:text-2xl border-b-2 pb-1 lg:pb-2 border-gray-800 w-fit font-bold tracking-wider">
+          About SRMIST
+        </h1>
+        <p className="text-sm md:text-base text-slate-500 font-medium text-justify">
+          SRM Institute of Science and Technology (formerly known as SRM
+          University) is one of the top ranking universities in India with over
+          20,000 students and 1,500 faculty, offering a wide range of
+          undergraduate, postgraduate and doctoral programs in Engineering,
+          Management, Medicine and Health sciences, Science and Humanities.
+        </p>
+        <Link
+          className="self-end"
+          href="https://srmrmp.edu.in/"
+          target={"_blank"}
+        >
+          <Button variant={"outline"}>More About SRM</Button>
+        </Link>
+      </div>
+      <div className="flex flex-col lg:items-center lg:w-full gap-4 my-10 lg:my-20">
+        <h1 className="text-lg md:text-xl lg:text-2xl border-b-2 pb-1 lg:pb-2 border-gray-800 w-fit font-bold tracking-wider">
+          About CSE
+        </h1>
+        <p className="text-sm md:text-base text-slate-500 font-medium text-justify">
+          The department of Computer Science and Engineering (CSE) at SRM
+          Institute of Science and Technology was established in the year 2004
+          with the aim of imparting quality education to students and bring out
+          the best in them. The key goal of the department is to provide best IT
+          infrastructure, world class learning and research environment, adopt
+          industry practices through industry collaborations and inculcate moral
+          and ethical values. With students hailing from all States and union
+          territories of India, the department was established to meet the
+          demand for well-qualified computer professionals. The promptness of
+          the students to learn makes it easier for the industry-trained
+          experienced faculty to produce top-notch engineers who are being
+          recruited by reputed companies all over the world. The department is
+          renowned for imparting state of the art undergraduate education and
+          preparing its students for real world challenges. The department is
+          accredited by NAAC as Grade A++ and placed by MHRD in category A. Our
+          alumni have done extremely well which include managing top companies,
+          designing revolutionary products, and contributing to fundamental
+          research.
+        </p>
+        <h1 className="font-bold tracking-wider self-start text-lg md:text-xl lg:text-2xl mt-10">
+          Courses Offered
+        </h1>
+        <p className="text-sm md:text-base text-slate-500 self-start font-medium text-left">
+          <p>B.Tech - Computer Science & Engineering </p>
+          <p>
+            B.Tech - CSE with specialization in Artificial Intelligence and
+            Machine Learning
+          </p>
+          <p>B.Tech - CSE with Specialization in Cloud Computing</p>
+          <p>B.Tech - CSE with Specialization in Gaming Technology</p>
+          <p>B.Tech - CSE with Specialization in Big Data Analytics </p>
+          <p>B.Tech - CSE with Specialization in Internet of Things </p>
+          <p>B.Tech - CSE with Specialization in Cyber Security </p>
+          <p>B.Tech - Computer Science and Business Systems</p>
+        </p>
+        <h1 className="font-bold tracking-wider self-start text-lg md:text-xl lg:text-2xl mt-10">
+          Vision
+        </h1>
+        <p className="text-sm md:text-base text-slate-500 font-medium text-justify">
+          To become a world class department in imparting high quality knowledge
+          and in providing students a unique learning and research experience in
+          Computer Science and Engineering.
+        </p>
+        <h1 className="text-lg md:text-xl lg:text-2xl mt-10 self-start font-bold tracking-wider">
+          Mission
+        </h1>
+        <ul className="list-disc self-start">
+          <li className="text-sm md:text-base text-slate-500 font-medium text-justify">
+            To impart knowledge in cutting edge technologies on part with
+            industrial standards.
+          </li>
+          <li className="text-sm md:text-base text-slate-500 font-medium text-justify">
+            To collaborate with renowned academic institutions in research and
+            development.
+          </li>
+          <li className="text-sm md:text-base text-slate-500 font-medium text-justify">
+            To instill societal and ethical responsibilities in all professional
+            activities.
+          </li>
+        </ul>
+        <Link
+          className="self-end"
+          href="https://srmrmp.edu.in/academics/department-of-computer-science-and-engineering/"
+          target={"_blank"}
+        >
+          <Button variant={"outline"}>More About CSE</Button>
+        </Link>
+      </div>
+    </section>
+  );
 }
 
 export default About;

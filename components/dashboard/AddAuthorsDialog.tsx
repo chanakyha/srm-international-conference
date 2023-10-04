0b1cdbe7b44676da5c1f8b49d421f7471966f33f
Jayanth Srinivasan <jayanthsrinivasan2003@gmail.com>
2023-10-04 08:38:51 +0530

    fix

--- components/dashboard/AddAuthorsDialog.tsx
+++ components/dashboard/AddAuthorsDialog.tsx
@@ -10,16 +10,24 @@ import {
 import { Button } from "../ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/backend/firebase";
+
+
 
 const AddAuthorsDialog = () => {
+
+  const handleAddAuthors = async(e:any) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const docRef = doc(db, "users", email);
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+    } else {
+      alert("User does not exist");
+    }
+  };
   return (
     <div>
       <Dialog>
@@ -32,15 +40,17 @@ const AddAuthorsDialog = () => {
           <DialogHeader>
             <DialogTitle>Add Authors</DialogTitle>
             <DialogDescription>
-              <section className="mt-4 flex flex-col gap-4">
+              <form
+                onSubmit={(e) => handleAddAuthors(e)}
+               className="mt-4 flex flex-col gap-4">
                 <div className="grid w-full items-center gap-1.5">
                   <Label htmlFor="email">Email</Label>
                   <Input type="email" id="email" placeholder="Email" />
                 </div>
                 <div>
-                  <Button className="w-full">Submit</Button>
+                  <Button type="submit" className="w-full">Add</Button>
                 </div>
-              </section>
+              </form>
             </DialogDescription>
           </DialogHeader>
         </DialogContent>

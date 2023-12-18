d9e9c2d6b69d470ea6c4c3d49c4594d8f2df4a06
chanakyha <vetrichanakyha2003@gmail.com>
2023-12-18 11:12:41 +0530

    Dated updated

--- components/landing/Info.tsx
+++ components/landing/Info.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Info() {
   return (
-    <div className="text-center bg-orange-500">
+    <div className="text-center bg-green-500">
       <p className="font-mono text-base font-semibold max-w-3xl mx-auto p-2">
         International conference ICACST&apos;23 would be conducted on
         29.12.2023.Kindly make payment on or before 16.12.2023

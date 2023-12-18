28eac893aaf80481820778920f93d92b9cf40e28
chanakyha <vetrichanakyha2003@gmail.com>
2023-12-18 11:12:10 +0530

    Date Updated

--- components/landing/Info.tsx
+++ components/landing/Info.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Info() {
   return (
-      <div className="text-center bg-orange-500">
-          <p className="font-mono text-base font-semibold">
-              In light of substantial rainfall caused by the cyclonic strom,{" "}
-              <br /> The conference ICACST&apos;23 date is postponed, The postponed date
-              will be informed shortly. Sorry for inconvenience.
-          </p>
-      </div>
+    <div className="text-center bg-orange-500">
+      <p className="font-mono text-base font-semibold max-w-3xl mx-auto p-2">
+        International conference ICACST&apos;23 would be conducted on
+        29.12.2023.Kindly make payment on or before 16.12.2023
+      </p>
+    </div>
   );
 }
 
-export default Info
+export default Info;
